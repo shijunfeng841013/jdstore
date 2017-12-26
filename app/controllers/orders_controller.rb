a8cd3a7d4f946b2e4e21e7262645e006309f0668
shijunfeng <johnshiluck@gmail.comgit>
2017-12-26 15:35:24 +0800

    add notify_cancel/ship function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -53,7 +53,7 @@ class OrdersController < ApplicationController
   def apply_to_cancel
     @order = Order.find_by_token(params[:id])
     OrderMailer.apply_cancel(@order).deliver!
-    flash[:notice] = '已提交申请'
+    flash[:notice] = "已提交申请"
     redirect_to :back
   end
   private
